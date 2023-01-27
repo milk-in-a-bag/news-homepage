953c3d434ff6fc122ad539d7ce59af4dc9174bc3
Kerry <bryanmayodi@gmail.com>
2023-01-27 21:55:21 +0300

    remaining transition for side menu

---
+++ app.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".fa-bars");
+const sideMenu =document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links");
+
+hamburger.addEventListener("click", (e)=>{
+    sideMenu.classList.toggle("active");
+})
+
+links.forEach(n=> n.addEventListener("click", (e)=>{
+    sideMenu.classList.remove("active");
+}));
