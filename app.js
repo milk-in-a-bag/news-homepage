4f41e50828c75e1c377237dd8f53578654b42a9d
Kerry <bryanmayodi@gmail.com>
2023-01-28 13:14:48 +0300

    done with sidebar

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const hamburger = document.querySelector(".bars");
 const sideMenu =document.querySelector(".nav-links");
+const xmark = document.querySelector(".fa-xmark");
 const links = document.querySelectorAll(".nav-links");
 
 hamburger.addEventListener("click", (e)=>{
     sideMenu.classList.toggle("active");
-})
-
+    xmark.classList.toggle("active");
+});
 links.forEach(n=> n.addEventListener("click", (e)=>{
     sideMenu.classList.remove("active");
+    xmark.classList.remove("active");
 }));
