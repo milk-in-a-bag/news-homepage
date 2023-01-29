567ff9da5a1a95143270b4753e5b37682eb18555
Kerry <bryanmayodi@gmail.com>
2023-01-29 12:41:43 +0300

    fixed the hamburger menu

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const hamburger = document.querySelector(".bars");
 const sideMenu =document.querySelector(".nav-links");
-const xmark = document.querySelector(".fa-xmark");
+const xmark = document.querySelector(".x");
 const links = document.querySelectorAll(".nav-links");
 
 hamburger.addEventListener("click", (e)=>{
     sideMenu.classList.toggle("active");
     xmark.classList.toggle("active");
 });
+xmark.addEventListener("click", (e)=>{
+    sideMenu.classList.remove("active");
+    xmark.classList.remove("active");
+})
 links.forEach(n=> n.addEventListener("click", (e)=>{
     sideMenu.classList.remove("active");
     xmark.classList.remove("active");
